006b9f61a7a06c21fef2b87e0dc07e594825c967
Huski-commando <gyan.mu@gmail.com>
2023-10-27 03:15:31 +0530

    got current user and username is coming from server

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,43 @@
 "use client";
 
 import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
 
-const Navbar = () => {
+const Navbar = ({ currentUser }) => {
   const { data, status } = useSession();
   const router = useRouter();
 
   if (status === "authenticated") {
     router.push("/");
   }
-  //   console.log(data);
+  // console.log(currentUser);
   return (
     <header className="w-full bg-slate-200">
       <nav className="max-w-6xl mx-auto flex justify-between py-6 font-semibold">
         <Link href="/">My-Work</Link>
 
         <div>
+          {currentUser ? (
+            <div className="flex gap-4  items-center">
+              {currentUser?.name}
+              <button onClick={() => signOut()}>Sign out</button>
+              <Image
+                src={currentUser?.image}
+                alt={currentUser?.name}
+                width={30}
+                height={30}
+                className="rounded-full cursor-pointer"
+              />
+            </div>
+          ) : (
+            <Link href="/login">Login</Link>
+          )}
+        </div>
+
+        {/* <div>
           {status === "unauthenticated" ? (
             <Link href="/login">Login</Link>
           ) : (
@@ -27,7 +46,7 @@ const Navbar = () => {
               <button onClick={() => signOut()}>Logout</button>
             </div>
           )}
-        </div>
+        </div> */}
       </nav>
     </header>
   );
--- src/utils/getCurrentUser.js
+++ src/utils/getCurrentUser.js
@@ -8,6 +8,24 @@ export async function getSession() {
 export default async function getCurrentUser() {
   try {
     const session = await getSession();
+
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return {
+      ...currentUser,
+    };
   } catch (error) {
     return null;
   }
