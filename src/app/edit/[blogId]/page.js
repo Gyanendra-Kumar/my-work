4af8f623503d0fa8d4f4d9eca27cabf050ee15a0
Huski-commando <gyan.mu@gmail.com>
2023-10-28 05:41:19 +0530

    finished update section

--- src/app/edit/[blogId]/page.js
+++ src/app/edit/[blogId]/page.js
@@ -1,26 +1,37 @@
+import EditForm from "@/components/EditForm";
 import getBlogById from "@/utils/getBlogsById";
 import getCurrentUser from "@/utils/getCurrentUser";
 
 const page = async ({ params }) => {
-  const currentUser = await getCurrentUser();
   const { blogId } = params;
+
+  const blog = await getBlogById(blogId);
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return null;
+  }
+
+  const { name, imageSrc, description } = blog;
 
   if (!blogId || typeof blogId !== "string") {
     return null;
   }
 
-  const blog = await getBlogById(blogId);
-
   if (!blog) {
     throw new Error("No blog found");
   }
   // console.log(blogId);
-  //   console.log(blog);
+  // console.log(blog);
 
   return (
     <div>
-      <p>Edit page</p>
-      <img src={blog?.imageSrc} />
+      <EditForm
+        name={blog?.name}
+        description={blog?.description}
+        blogId={blog?.id}
+        imageSrc={blog?.imageSrc}
+      />
     </div>
   );
 };
---
+++ src/components/EditForm.js
@@ -0,0 +1,97 @@
+"use client";
+
+import ImageUpload from "@/components/ImageUpload";
+import Input from "@/components/Input";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const EditForm = ({ name, description, imageSrc, blogId }) => {
+  const initialState = {
+    name: name,
+    imageSrc: imageSrc,
+    description: description,
+  };
+
+  const [state, setState] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "unauthenticated") {
+    return router.push("/");
+  }
+
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const customValue = (id, value) => {
+    setState((prevValues) => ({
+      ...prevValues,
+      [id]: value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.put(`/api/blogs/${blogId}`, state);
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  return (
+    <form className="w-[600px] h-[700px] mx-auto py-8" onSubmit={onSubmit}>
+      <div>
+        <ImageUpload
+          value={state.imageSrc}
+          onChange={(value) => customValue("imageSrc", value)}
+        />
+      </div>
+
+      <div className="flex flex-col justify-center h-[450px] w-[350px] mx-auto gap-2">
+        <div>
+          <Input
+            name="name"
+            id="name"
+            placeholder="Blog Header"
+            type="text"
+            value={state.name}
+            onChange={handleChange}
+          />
+          <Input
+            name="description"
+            id="description"
+            placeholder="Blog Description"
+            type="text"
+            value={state.description}
+            onChange={handleChange}
+          />
+        </div>
+        <button
+          type="submit"
+          disabled={isLoading}
+          className="bg-slate-200 py-4"
+        >
+          Update
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default EditForm;
